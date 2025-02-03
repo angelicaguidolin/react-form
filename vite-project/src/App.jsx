ec7cec4db7956322401ddc79540a08c5c45a9eed
angelica <guidolinangelica66@gmail.com>
2025-02-03 16:31:25 +0100

    feat add first milestone

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,1 +1,18 @@
-export default  function App(){}
+export default  function App(){
+
+return(
+  <div>
+    <h1>ALbum da Comprare</h1>
+    <ul>
+      <li>Tunnel</li>
+      <li>Dio lo sa</li>
+      <li>è finita la pace</li>
+      <li>Containers</li>
+      <li>Mr simpatia</li>
+      <li>Noi, loro, gli altri</li>
+      <li>Mi fist</li>
+    </ul>
+  </div>
+)
+
+}
